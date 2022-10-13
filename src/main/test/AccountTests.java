8b268735c1995c6a877c8f7e14d6d933d46e043a
EazazJ1 <ejakda2584@conestogac.on.ca>
2022-10-13 12:10:45 -0400

    Added a new line and jdk to make sure project is running

---
+++ src/main/test/AccountTests.java
@@ -0,0 +1,18 @@
+package main.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+import main.java.Account;
+
+public class AccountTests {
+    Account testAccount = new Account(12345,54321,100,1000);
+    @Test
+    public void testPINTrue() {
+        boolean testResult = testAccount.validatePIN(54321);
+
+        assertTrue(testResult);
+
+    }
+}
