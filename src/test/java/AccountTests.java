8950e4c380a89fddb3ca5714b7dc8117875c965a
Rachel Le <71084940+Rachel10011@users.noreply.github.com>
2022-10-14 15:22:20 -0400

    Implemented unit tests for Account class.

--- src/test/java/AccountTests.java
+++ src/test/java/AccountTests.java
@@ -8,36 +8,60 @@ public class AccountTests {
     Account testAccount = new Account(12345,54321,100,1000);
 
     @Test
-    public void testPINTrue() {
-        boolean testResult = testAccount.validatePIN(54321);
-
-        assertTrue(testResult);
-
-    }
-    @Test
-    public void testPINFalse() {
-        boolean testResult = testAccount.validatePIN(5432);
-
-        assertFalse(testResult);
-
-    }
-    @Test
-    public void testGetAvailableBalance() {
-
-
-        Account newAccount = new Account(12345, 54321, 1000, 12000);
-        double correctAmount = 1000;
-        double result = newAccount.getAvailableBalance();
-
-        assertEquals(correctAmount, result, 0);
+    public void T001_ValidatePIN_54321_True() {
+        boolean actual = testAccount.validatePIN(54321);
+        assertTrue(null, actual);
     }
 
     @Test
-    public void testGetTotalBalance() {
-
-        double correctAmount = 1000;
-        double result = testAccount.getTotalBalance();
-
-        assertEquals(correctAmount, result, 0);
+    public void T002_ValidatePIN_12345_False() {
+        boolean actual = testAccount.validatePIN(12345);
+        assertFalse(null, actual);
     }
+
+    @Test
+    public void T003_GetAvailableBalance_100() {
+        double expected = 100;
+        double actual = testAccount.getAvailableBalance();
+        assertTrue(null, actual == expected);
+    }
+
+    @Test
+    public void T004_GetTotalBalance_1000() {
+        double expected = 1000;
+        double actual = testAccount.getTotalBalance();
+        assertTrue(null, actual == expected);
+    }
+    @Test
+    public void T005_Credit_10_Expect_TotalBalance_1100() {
+        double expected = 1010;
+        testAccount.credit(10);
+        double actual = testAccount.getTotalBalance();
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void T006_Debit_10_Expect_AvailableBalance_90() {
+        double expected = 90;
+        testAccount.debit(10);
+        double actual = testAccount.getAvailableBalance();
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void T007_Debit_10_Expect_TotalBalance_990() {
+        double expected = 990;
+        testAccount.debit(10);
+        double actual = testAccount.getTotalBalance();
+        assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void T008_GetAccountNumber_12345() {
+        double expected = 12345;
+        double actual = testAccount.getAccountNumber();
+        assertEquals(expected, actual, 0);
+    }
+
+
 }
