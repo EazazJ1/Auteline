09a8d2777de909345f89e6df3487c8d6e8078c95
EazazJ1 <ejakda2584@conestogac.on.ca>
2022-11-21 16:17:21 -0500

    Fixed BankDatabase tests to use the actual database. Updated pom.xml to include dependency for mysql.

--- src/test/java/BankDatabaseTests.java
+++ src/test/java/BankDatabaseTests.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import MainApp.BankDatabase;
 
+
 public class BankDatabaseTests {
 
     @Test
@@ -24,33 +25,40 @@ public class BankDatabaseTests {
     public void T003_getAvailableBalance_OneThousandAvailableBalance_ReturnsOneThousand() {
         BankDatabase testDatabase = new BankDatabase();
 
+        double initResult = testDatabase.getAvailableBalance(12345);
+
         double result = testDatabase.getAvailableBalance(12345);
-        assertTrue(result == 1000);
+        assertTrue(result == initResult);
     }
 
     @Test
     public void T004_getTotalBalance_OneThousandTwoHundredTotalBalance_ReturnsOneThousandTwoHundredTotalBalance() {
         BankDatabase testDatabase = new BankDatabase();
 
+        double initResult = testDatabase.getAvailableBalance(12345);
         double result = testDatabase.getAvailableBalance(12345);
-        assertTrue(result == 1000);
+        assertTrue(result == initResult);
     }
 
     @Test
     public void T005_Credit_OneHundredCredit_ThirteenHundredTotalBalance() {
         BankDatabase testDatabase = new BankDatabase();
+        double initResult = testDatabase.getTotalBalance(12345);
+
         testDatabase.credit(12345, 100);
 
         double result = testDatabase.getTotalBalance(12345);
-        assertTrue(result == 1300);
+        assertTrue(result == initResult + 100);
     }
 
     @Test
     public void T006_Debit_OneHundredDebit_NineHundredTotalBalance() {
         BankDatabase testDatabase = new BankDatabase();
+
+        double initResult = testDatabase.getAvailableBalance(12345);
         testDatabase.debit(12345, 100);
 
         double result = testDatabase.getAvailableBalance(12345);
-        assertTrue(result == 900);
+        assertTrue(result == initResult - 100);
     }
 }
